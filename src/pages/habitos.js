81544f84281186ec9ce49ad8ae0136078b24e127
MarceloMosl <marcelomosl@icloud.com>
2022-12-19 20:09:36 -0300

    Requisito: Implementada Função de Incluir novos Habitos e Pegar os Habitos do usuario na API e mostra-los na tela

--- src/pages/habitos.js
+++ src/pages/habitos.js
@@ -1,144 +1,192 @@
-import neymar from "./../assets/neymar.jpg"
-import styled from "styled-components"
-import React, {useContext} from "react"
-import { AuthToken } from "../context/Context"
+import neymar from "./../assets/neymar.jpg";
+import styled from "styled-components";
+import React, { useContext, useEffect } from "react";
+import { AuthToken } from "../context/Context";
+import axios from "axios";
+
+export default function Habits() {
+    const [state, setState] = React.useState(false);
+    const [name, setName] = React.useState("")
+    const weekDays = [
+        "Segunda",
+        "Terça",
+        "Quarta",
+        "Quita",
+        "Sexta",
+        "Sabado",
+        "Domingo",
+    ];
+    const [selecionados, setSelecionados] = React.useState([]);
+    const [days, setDays] = React.useState([]);
+    const [habitos, setHabitos] = React.useState([])
+    const { token } = useContext(AuthToken);
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", { headers: { "Authorization": `Bearer ${token.token}` } })
+        promise.then((res) => {
+            setHabitos(res.data)
+            console.log(res.data)
+        })
+        promise.catch((err) => console.log(err.response.data))
+    }, [days])
 
 
-
-export default function Habits () {
-    const [state, setState] = React.useState(false)
-    const days =  ["Segunda", "Terça", "Quarta", "Quita", "Sexta", "Sabado", "Domingo"]
-    const [selecionados, setSelecionados] = React.useState([])
-
-    const { token } = useContext(AuthToken)
+    function enviarHabito (a) {
+        a.preventDefault()
+        const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", {name,days},{ headers: { "Authorization": `Bearer ${token.token}` }})
+        promise.then((res) => {console.log(res.data)
+            setDays([])
+        })
+        promise.catch((err) => console.log(err.response.data))
+    }
 
     return (
-    <>
-        <Header>
-            <h1>TrackIt</h1>
-            <img src={neymar} alt="Neymaru"></img>
-        </Header>
+        <>
+            <Header>
+                <h1>TrackIt</h1>
+                <img src={token.image} alt="Neymaru"></img>
+            </Header>
 
-        <Add>
-            <h1>Meus hábitos</h1>
-            <button onClick={() => state == true ? setState(false) : setState(true)}> + </button>
-        </Add>
-        <Habito visible={state}>
-                <input type="text" placeholder="nome do habito" required/>
-            <div>
-                {days.map((a,b) => selecionados.includes(a) ? <Butao select={"true"} type="button">{a[0]}</Butao> : <Butao select={"false"} onClick={() => setSelecionados([...selecionados, a])} type="button">{a[0]}</Butao>  )}
-            </div>
-            <section>
-                <Cancelar type="reset" onClick={ () => setSelecionados([])}>Cancelar</Cancelar>
-                <Enviar type="submit">Enviar</Enviar>
-            </section>
+            <Add>
+                <h1>Meus hábitos</h1>
+                <button
+                    onClick={() => (state == true ? setState(false) : setState(true))}
+                >+
+                </button>
+            </Add>
+            <Habito onSubmit={(a) => enviarHabito(a)} visible={state}>
+                <input type="text" onChange={(e) => setName(e.target.value)} placeholder="nome do habito" required />
+                <div>
+                    {weekDays.map((a, b) =>
+                        selecionados.includes(a) ? (
+                            <Butao select={"true"} type="button" disabled={true}>
+                                {a[0]}
+                            </Butao>
+                        ) : (
+                            <Butao
+                                select={"false"}
+                                onClick={() => {
+                                    setSelecionados([...selecionados, a])
+                                    setDays([...days, b+1])
+                                }}
+                                type="button"
+                            >
+                                {a[0]}
+                            </Butao>
+                        )
+                    )}
+                </div>
+                <section>
+                    <Cancelar type="reset" onClick={() => {
+                        setSelecionados([])
+                        setDays([])
+                    }}>
+                        Cancelar
+                    </Cancelar>
+                    <Enviar type="submit" onClick={() => {
+                        setSelecionados([])
+                    }}>Enviar</Enviar>
+                </section>
+            </Habito>
 
-        </Habito>
+            <Content>
+                {habitos.length == 0 
+                ? <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
+                : habitos.map((a) => <p>{a.name}</p>)}
+            </Content>
 
-        <Content>
-            <p>
-            Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
-            </p>
-        </Content>
-    
-        <footer></footer>
-    </>
-    
-    )
+            <footer></footer>
+        </>
+    );
 }
 
 const Header = styled.div`
-display: flex;
-justify-content: space-between;
-padding: 20px;
-background-color: #126BA5;
-color: white;
-align-items: center;
-font-size: 39px;
-font-family: "Playball";
-height: 70px;
-img{
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  background-color: #126ba5;
+  color: white;
+  align-items: center;
+  font-size: 39px;
+  font-family: "Playball";
+  height: 70px;
+  img {
     width: 51px;
     height: 51px;
     border-radius: 98.5px;
-}
-`
+  }
+`;
 const Add = styled.div`
-font-family: "Lexend Deca";
-font-size: 23px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 30px;
-color:#126BA5;
-button{
-    background-color: #52B6FF;
+  font-family: "Lexend Deca";
+  font-size: 23px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+  color: #126ba5;
+  button {
+    background-color: #52b6ff;
     border: none;
     color: white;
     width: 40px;
     height: 35px;
     border-radius: 5px;
-}
-`
+  }
+`;
 const Content = styled.div`
-font-family: "Lexend Deca";
-font-size: 18px;
-color: #666666;
-padding: 20px;
-`
+  font-family: "Lexend Deca";
+  font-size: 18px;
+  color: #666666;
+  padding: 20px;
+`;
 const Habito = styled.form`
-display: ${(props) => (props.visible ? "flex" : "none")};
-flex-direction: column;
-padding: 20px;
-margin: auto;
-color: #D4D4D4;
-input{
+  display: ${(props) => (props.visible ? "flex" : "none")};
+  flex-direction: column;
+  padding: 20px;
+  margin: auto;
+  color: #d4d4d4;
+  input {
     height: 45px;
     border: none;
-    border: 1px solid #D4D4D4;
-}
-div{
+    border: 1px solid #d4d4d4;
+  }
+  div {
     display: flex;
     margin: auto;
     margin-top: 15px;
     gap: 20px;
-}
-section{
+  }
+  section {
     display: flex;
     justify-content: flex-end;
     gap: 10px;
-    button{
-        width: 84px;
-        height: 50px;
+    button {
+      width: 84px;
+      height: 50px;
     }
-}
-`
+  }
+`;
 const Cancelar = styled.button`
-color: #52B6FF;
-background-color: white;
-border: none;
-
-`
+  color: #52b6ff;
+  background-color: white;
+  border: none;
+`;
 const Enviar = styled.button`
-color: white;
-background-color: #52B6FF;
-border: none;
-border-radius: 5px;
-
-`
-const Butao = styled.button`  
-        display: flex;
-        justify-content: center;
-        text-align: center;
-        align-items: center;
-        color: #D4D4D4;
-        background-color: ${(props) => (props.select ===  "true" ? "grey" : "white")};
-        border: none;
-        border: 1px solid #D4D4D4;
-        width: 25px;
-        height: 30px;
-        margin-bottom: 20px;
-
-
-
-`
+  color: white;
+  background-color: #52b6ff;
+  border: none;
+  border-radius: 5px;
+`;
+const Butao = styled.button`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  color: #d4d4d4;
+  background-color: ${(props) => (props.select === "true" ? "grey" : "white")};
+  border: none;
+  border: 1px solid #d4d4d4;
+  width: 25px;
+  height: 30px;
+  margin-bottom: 20px;
+`;
